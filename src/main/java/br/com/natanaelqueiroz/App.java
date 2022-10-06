3a5d4d34b564ad0d11e2d4406dbd6e771e94a254
NatanaelQueiroz <natanqueiroz77@gmail.com>
2022-10-06 05:45:01 -0300

    fazendo aplicação rodar no java 11

--- src/main/java/br/com/natanaelqueiroz/App.java
+++ src/main/java/br/com/natanaelqueiroz/App.java
@@ -8,6 +8,16 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        /*
+         mostre()
+         leia()
+         */
+
+         int numero1 = 3;
+         var numero2 = 5;
+
+         int soma = numero1 + numero2;
+
+        System.out.println( "O resultado da soma é " + soma );
     }
 }
